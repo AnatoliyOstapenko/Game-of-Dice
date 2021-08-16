4cce4ee1d088e24fcd948e101db77f2094edb45b
Anatoliy Ostapenko <anatoliy.ostapenko@gmail.com>
2021-08-16 20:45:36 +0300

    Initial Commit

---
+++ Game of Dice/Game_of_DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_of_DiceApp.swift
+//  Game of Dice
+//
+//  Created by MacBook on 16.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct Game_of_DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
